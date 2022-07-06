277c7724af49ea07d585eb738f3b26fad8b947a6
halil_yaris <halilibrahimyaris34@gmail.com>
2022-07-06 11:37:53 +0300

    Project Created

---
+++ springRestApi/springRestApi/src/test/java/com/example/springRestApi/SpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
