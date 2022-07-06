34928ca97e3d45f81fa727dd12409deee50006ad
halil_yaris <halilibrahimyaris34@gmail.com>
2022-07-06 11:57:53 +0300

    Model Defined

---
+++ springRestApi/springRestApi/src/main/java/com/example/springRestApi/domain/Address.java
@@ -0,0 +1,11 @@
+package com.example.springRestApi.domain;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String country;
+    private String city;
+    private String postCode;
+
+}
---
+++ springRestApi/springRestApi/src/main/java/com/example/springRestApi/domain/Gender.java
@@ -0,0 +1,4 @@
+package com.example.springRestApi.domain;
+
+public class Gender {
+}
