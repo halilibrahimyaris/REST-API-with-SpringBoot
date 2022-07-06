80fd45e7a77cd7c4c7b8c5821f61ca685ff36158
halil_yaris <halilibrahimyaris34@gmail.com>
2022-07-06 14:05:29 +0300

    Sample MongoDB queries

--- springRestApi/springRestApi/src/main/java/com/example/springRestApi/SpringRestApiApplication.java
+++ springRestApi/springRestApi/src/main/java/com/example/springRestApi/SpringRestApiApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,10 +24,26 @@ public class SpringRestApiApplication {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 	@Bean
-	CommandLineRunner runner(StudentRepository studentRepository){
+	CommandLineRunner runner(StudentRepository studentRepository,
+							 MongoTemplate mongoTemplate){
+		String email= "halil@gmail.com";
 		return args -> {
-			Student student1= new Student("halil","yaris","halil@gmail.com",Gender.MALE,new Address(), List.of("computer science","maths"), BigDecimal.TEN, LocalDateTime.now());
-			studentRepository.insert(student1);
+			Student student1= new Student("halil","yaris",email,Gender.MALE,new Address(), List.of("computer science","maths"), BigDecimal.TEN, LocalDateTime.now());
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+			List<Student> students = mongoTemplate.find(query,Student.class);
+
+		 	if(students.size()>1){
+				 throw new IllegalStateException("Found many student with email");
+			}
+			 if(students.isEmpty()){
+				 System.out.println("Inserted student"+ student1);
+				 studentRepository.insert(student1);
+			 }
+			 else {
+				 System.out.println("Student already exist");
+			 }
+
 		};
 	}
 
